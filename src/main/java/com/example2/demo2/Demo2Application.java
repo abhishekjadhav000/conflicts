10ee74c11373a7a5a9edc449dfa6e5a440ea4818
abhishekjadhav000 <abhishek000jadhav@gmail.com>
2025-03-13 10:04:44 +0530

    Update Demo2Application.java

--- src/main/java/com/example2/demo2/Demo2Application.java
+++ src/main/java/com/example2/demo2/Demo2Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Demo2Application {
 
 	public static void main(String[] args) {
+		int x=100;
 		SpringApplication.run(Demo2Application.class, args);
 	}
 
